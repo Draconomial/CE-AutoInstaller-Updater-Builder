498d44bded7bdb4ac1be75553787af936951bc94
Copacetic <Draconomial@users.noreply.github.com>
2025-07-09 17:14:19 -0700

    fixes and imrovements
    Quoting paths
    Broken %INPUT% reuse
    delayed expansion in loops
    exit vs exit /b
    Missing quotes
    central build.log

--- CE-AutoInstaller-Updater-Builder.bat
+++ CE-AutoInstaller-Updater-Builder.bat
@@ -1,127 +1,150 @@
 @Echo off
+setlocal enabledelayedexpansion
 echo HobGoblin's Fast Lazy Installer and Updater for Combat Extended :D
+
+:: Check if winget is available
+where winget >nul 2>&1
+if %errorlevel% neq 0 (
+    echo winget is not installed or not in PATH. Please install App Installer from the Microsoft Store.
+    pause
+    exit /b 1
+)
 
 :: Check if dotnet is installed
 dotnet --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo .NET SDK is not installed. Attempting to install it using winget...
-    winget install --id=Microsoft.dotnet.SDK.9
+    winget install --id Microsoft.DotNet.SDK.9 --source winget
     if %errorlevel% neq 0 (
-        echo .NET SDK is not installed. Please download and install it from https://dotnet.microsoft.com/download/dotnet/9.0 manually, and then re-run the script.
+        echo .NET SDK installation failed. Please install it manually from https://dotnet.microsoft.com/download/dotnet/9.0
         pause
         exit /b 1
     )
 )
 
-:: Continue to Check if Git is installed
+:: Check if Git is installed
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git is not installed. Attempting to install Git using winget...
+    echo Git is not installed. Attempting to install it using winget...
     winget install --id Git.Git -e --source winget
     if %errorlevel% neq 0 (
-        echo Failed to install Git. Please install it manually from https://git-scm.com/ and re-run the script.
+        echo Git installation failed. Please install it manually from https://git-scm.com/
         pause
         exit /b 1
     )
 )
 
-:: Continue with Mods folder verifier
+:: Check if we're inside the Mods folder
+call :ModsFolderVerifier
 
-CALL :ModsFolderVerifier
+:: Set this to true if you only want to update and not install new mods
+set "DONTINSTALL=false"
 
-:: Set this to true if you only want to update and don't want to install mods!
-set DONTINSTALL=false
-
-:: Simply delete the line below with the mods you don't want to install then run to auto-install and auto-update!
-CALL :GitUpdate CombatExtended https://github.com/CombatExtended-Continued/CombatExtended.git
+:: Auto-install or update mods
+call :GitUpdate CombatExtended https://github.com/CombatExtended-Continued/CombatExtended.git
 
 echo.
-ECHO If you're running this for the first time, run it again to build the assembly files! Every time you run this Updater after the initial run, it will pull the latest dev files and rebuild the assembly files.
+echo If you're running this for the first time, run it again to build the assembly files!
+echo After the first run, this will pull the latest dev files and rebuild automatically.
 pause
-EXIT 0
+exit /b 0
 
 :GitUpdate
 echo.
+set "ModFolderName=%~1"
+set "RepoURL=%~2"
 
-set ModFolderName=%~1
-if exist %~1-master/ set ModFolderName=%~1-master
+if exist "%~1-master\" (
+    set "ModFolderName=%~1-master"
+)
 
-if exist %ModFolderName%/ (
+if exist "%ModFolderName%\" (
     echo Updating %ModFolderName%...
-    cd %ModFolderName%
-    if exist .git (
+    pushd "%ModFolderName%"
+    if exist ".git" (
         git reset --hard HEAD
         git fetch
         git pull --rebase
         echo Building .sln files in the Source directory...
-        for /r %%I in (Source\*.sln) do (
+        for /r "Source" %%I in (*.sln) do (
             echo Building %%~nI...
             pushd "%%~dpI"
-            dotnet build "%%~nxI"
+            dotnet build "%%~nxI" >> "build.log" 2>&1
             popd
         )
-        cd ..
+        popd
     ) else (
-        cd ..
-        if %DONTINSTALL%==true EXIT /B 0
-        echo .git not found in /%ModFolderName%/.
-        set /P INPUT=Would you like to DELETE /%ModFolderName%/ and reinstall correctly for auto-update? [IAMSURE/n]: 
-        CALL :GitRemoveInstall %ModFolderName% %~2
+        popd
+        if /I "%DONTINSTALL%"=="true" exit /b 0
+        echo .git not found in "%ModFolderName%".
+        set "INPUT="
+        set /P INPUT=Would you like to DELETE "%ModFolderName%" and reinstall correctly for auto-update? [IAMSURE/n]: 
+        call :GitRemoveInstall "%ModFolderName%" "%RepoURL%" "!INPUT!"
+        set "INPUT="
     )
 ) else (
-    if %DONTINSTALL%==true EXIT /B 0
-    set /P INPUT=/Mods/%ModFolderName%/ folder not found. Would you like to install? [y/n]: 
-    CALL :GitInstall %~2
+    if /I "%DONTINSTALL%"=="true" exit /b 0
+    set "INPUT="
+    set /P INPUT=Folder "%ModFolderName%" not found. Would you like to install it? [y/n]: 
+    if /I "!INPUT!"=="y" (
+        call :GitInstall "%RepoURL%"
+    )
     echo Building .sln files in the Source directory...
     for /r "Source" %%I in (*.sln) do (
         echo Building %%~nI...
         pushd "%%~dpI"
-        dotnet build "%%~nxI"
+        dotnet build "%%~nxI" >> "build.log" 2>&1
         popd
     )
+    set "INPUT="
 )
 
-EXIT /B 0
+exit /b 0
 
 :GitOnlyCheckInstalled
 echo.
-echo Checking if %~1 installed...
-set FolderExists=N
-if exist %~1 set FolderExists=Y
-if exist %~1-master set FolderExists=Y
-if %FolderExists%==N (
+echo Checking if %~1 is installed...
+set "FolderExists=N"
+if exist "%~1" set "FolderExists=Y"
+if exist "%~1-master" set "FolderExists=Y"
+if /I "%FolderExists%"=="N" (
+    set "INPUT="
     set /P INPUT=/Mods/%~1/ folder not found. Would you like to install? [y/n]: 
-    CALL :GitInstall %~2
+    if /I "!INPUT!"=="y" (
+        call :GitInstall "%~2"
+    )
     echo Building .sln files in the Source directory...
     for /r "Source" %%I in (*.sln) do (
         echo Building %%~nI...
         pushd "%%~dpI"
-        dotnet build "%%~nxI"
+        dotnet build "%%~nxI" >> "build.log" 2>&1
         popd
     )
-) else echo %~1 is installed.
+    set "INPUT="
+) else (
+    echo %~1 is installed.
+)
 
-EXIT /B 0
+exit /b 0
 
 :GitInstall
-if /I %INPUT%==y git clone %~1
-set INPUT=n
-EXIT /B 0
+git clone %~1
+exit /b 0
 
 :GitRemoveInstall
-if /I %INPUT%==IAMSURE ( 
-    echo Deleting and reinstalling mod /%~1/...
-    RMDIR /S /Q "%~1"
+:: args: %~1 = folder, %~2 = repo URL, %~3 = confirmation input
+if /I "%~3"=="IAMSURE" (
+    echo Deleting and reinstalling mod %~1...
+    rmdir /S /Q %~1
     git clone %~2
-    set INPUT=n
 )
-EXIT /B 0
+exit /b 0
 
 :ModsFolderVerifier
-for %%I in (.) do set CurrDirName=%%~nxI
-if NOT %CurrDirName%==Mods (
-    echo Please place me in your RimWorld/Mods/ folder to run!
+for %%I in (.) do set "CurrDirName=%%~nxI"
+if /I not "!CurrDirName!"=="Mods" (
+    echo Please place this script inside your RimWorld/Mods/ folder.
     pause
-    EXIT 0
+    exit /b 1
 )
-EXIT /B 0
+exit /b 0
